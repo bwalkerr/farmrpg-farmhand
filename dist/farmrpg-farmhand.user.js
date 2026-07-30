3067d0c47b1f19aeebf785ba9defd424fd1b3a62
bwalkerr <134301991+bwalkerr@users.noreply.github.com>
2026-07-30 07:33:59 -0500

    chore: build 1.2.0

--- dist/farmrpg-farmhand.user.js
+++ dist/farmrpg-farmhand.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name Farm RPG Farmhand
 // @description Farmhand for Farm RPG (fork of anstosa/farmrpg-farmhand) — inventory cap tracker, dependable perk automation with an on-screen indicator, mining support, and notification fixes
-// @version 1.1.12
+// @version 1.2.0
 // @author Ansel Santosa <568242+anstosa@users.noreply.github.com>
 // @match https://farmrpg.com/*
 // @match https://www.farmrpg.com/*
 // @match https://alpha.farmrpg.com/*
 // @connect github.com
 // @connect raw.githubusercontent.com
-// @downloadURL https://raw.githubusercontent.com/bwalkerr/farmrpg-farmhand/reed-mods/dist/farmrpg-farmhand.user.js
+// @downloadURL https://raw.githubusercontent.com/bwalkerr/farmrpg-farmhand/mobile/dist/farmrpg-farmhand.user.js
 // @grant GM.deleteValue
 // @grant GM.getValue
 // @grant GM.listValues
@@ -18,7 +18,7 @@
 // @icon https://www.google.com/s2/favicons?sz=64&domain=farmrpg.com
 // @license MIT
 // @namespace https://github.com/anstosa/farmrpg-farmhand
-// @updateURL https://raw.githubusercontent.com/bwalkerr/farmrpg-farmhand/reed-mods/dist/farmrpg-farmhand.meta.js
+// @updateURL https://raw.githubusercontent.com/bwalkerr/farmrpg-farmhand/mobile/dist/farmrpg-farmhand.meta.js
 // ==/UserScript==
 
 /******/ (() => { // webpackBootstrap
@@ -7296,7 +7296,7 @@ const isVersionHigher = (test, current) => {
     }
     return false;
 };
-const currentVersion = normalizeVersion( true && "1.1.12" !== void 0 ? "1.1.12" : "1.0.0");
+const currentVersion = normalizeVersion( true && "1.2.0" !== void 0 ? "1.2.0" : "1.0.0");
 (0, notifications_1.registerNotificationHandler)(notifications_1.Handler.CHANGES, () => __awaiter(void 0, void 0, void 0, function* () {
     var _a, _b;
     const response = yield (0, requests_1.corsFetch)(api_1.CHANGELOG_URL);
